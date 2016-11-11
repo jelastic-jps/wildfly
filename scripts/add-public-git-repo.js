9f72d4eff52047f4bef86e432c3abe59436eccb0
Ruslan Synytsky <siruslan@gmail.com>
2016-11-11 21:33:21 +0100

    Create add-public-git-repo.js

---
+++ scripts/add-public-git-repo.js
@@ -0,0 +1,26 @@
+//@auth
+//@req(name, url, branch)
+
+var params = {
+   name: name,
+   envName: "${env.appid}",
+   env: "${env.envName}",
+   nodeId: "${nodes.build.first.id}",
+   session: session,
+   type: "git",
+   context: "ROOT",
+   url: url,
+   branch: branch,
+   keyId: null,
+   login: null,
+   password: null,
+   autoupdate: false,
+   interval: 1,
+   autoResolveConflict: true
+}
+
+//add and build the project 
+resp = jelastic.env.build.AddProject(params.envName, params.session, params.nodeId, params.name, params.type, params.url, params.keyId, params.login, params.password, params.env, params.context, params.branch, params.autoupdate, params.interval, params.autoResolveConflict);
+if (resp.result != 0) return resp;
+resp = jelastic.env.build.BuildProject(params.envName, params.session, params.nodeId, resp.id);
+return resp;
