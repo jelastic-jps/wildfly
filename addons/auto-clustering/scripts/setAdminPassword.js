bf4472832316cceda497fbde97fe7c33da001d17
Dmytro Zubelevych <dz@jelastic.com>
2018-04-06 18:06:54 +0300

    publishing manifest for certified images

---
+++ addons/auto-clustering/scripts/setAdminPassword.js
@@ -0,0 +1,18 @@
+import com.hivext.api.core.utils.Transport;
+import com.hivext.api.development.Scripting;
+import com.hivext.api.utils.Random;
+
+var SOURCE_NODE = "${settings.masterId}";
+
+var PASS_PATH = "/opt/wildfly/standalone/configuration/.pswd" ;
+
+var resp = jelastic.env.file.Read('${env.envName}', session, PASS_PATH, null, null, SOURCE_NODE);
+
+if (resp.result != 0) return resp;
+
+var resetPasswordCmd = "/opt/wildfly/bin/add-user.sh" ;
+
+var adminUserCredentials = "admin " + resp.body.replace(/\n/g, '') + "  2>&1";
+
+jelastic.env.control.ExecCmdById('${env.envName}', session, SOURCE_NODE, toJSON([{ "command": "/usr/bin/rm -f", "params": PASS_PATH }]), false, "root");
+return jelastic.env.control.ExecCmdByGroup('${env.envName}', session, "ctrl", toJSON([{ "command": resetPasswordCmd, "params": adminUserCredentials }]), false, false, "root");
