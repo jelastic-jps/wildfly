73a2b2d4dd2a7d770a1d7936a3f3cf6150f7b125
DmytroZubelevych <31444413+DmytroZubelevych@users.noreply.github.com>
2018-04-18 12:20:42 +0300

    Update setAdminPassword.js

--- addons/auto-clustering/scripts/setAdminPassword.js
+++ addons/auto-clustering/scripts/setAdminPassword.js
@@ -2,7 +2,7 @@ import com.hivext.api.core.utils.Transport;
 import com.hivext.api.development.Scripting;
 import com.hivext.api.utils.Random;
 
-var SOURCE_NODE = "${settings.masterId}";
+var SOURCE_NODE = "${nodes.cp.master.id}";
 
 var PASS_PATH = "/opt/wildfly/standalone/configuration/.pswd" ;
 
@@ -12,7 +12,8 @@ if (resp.result != 0) return resp;
 
 var resetPasswordCmd = "/opt/wildfly/bin/add-user.sh" ;
 
-var adminUserCredentials = "admin " + resp.body.replace(/\n/g, '') + "  2>&1";
+var adminUserCredentials = "admin " + resp.body.replace(new RegExp("\\n", "g"), "");
 
 jelastic.env.control.ExecCmdById('${env.envName}', session, SOURCE_NODE, toJSON([{ "command": "/usr/bin/rm -f", "params": PASS_PATH }]), false, "root");
+
 return jelastic.env.control.ExecCmdByGroup('${env.envName}', session, "ctrl", toJSON([{ "command": resetPasswordCmd, "params": adminUserCredentials }]), false, false, "root");
